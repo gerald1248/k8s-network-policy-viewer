65f248fa1862881f6104171930f70fd8337028f5
Gerald Schmidt <gerald1248@gmail.com>
2018-11-15 13:34:35 +0000

    tweaked HTML output

--- server.go
+++ server.go
@@ -50,28 +50,43 @@ func handleGet(w *http.ResponseWriter, r *http.Request) {
 		log.Printf("Graphviz conversion failed: %s\n", err)
 	}
 
+	isolatedPercentage := 100-percentage
+	colorClass := "bg-success"
+	if isolatedPercentage < 50 {
+		colorClass = "bg-danger"
+	} else if isolatedPercentage < 75 {
+		colorClass = "bg-warning"
+	}
+
 	// TODO: move to template
 	fmt.Fprintf(*w, `
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Network policy viewer</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
   <body>
     <div class="container">
       <h1>Network policy viewer</h1>
       <div>%s</div>
-    </div>
-    <div class="progress">
-      <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="%d" aria-valuemin="0" aria-valuemax="100">%d%% isolated</div>
+      <div class="progress">
+        <div class="progress-bar %s" style="width: %d%%" role="progressbar" aria-valuenow="%d" aria-valuemin="0" aria-valuemax="100">%d%% isolated</div>
+      </div>
     </div>
   </body>
 </html>`,
 	strings.Replace(svg.String(), "Times,serif", "sans-serif", -1),
-	100-percentage)
+	colorClass,
+	isolatedPercentage,
+	isolatedPercentage,
+	isolatedPercentage)
 }
 
 func handlePost(w *http.ResponseWriter, r *http.Request) {
