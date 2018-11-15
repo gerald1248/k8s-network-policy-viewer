9fffbdc12eb330daa29f0940697515f7f060800b
Gerald Schmidt <gerald1248@gmail.com>
2018-11-15 22:28:11 +0000

    added page template

---
+++ page.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func page(s string) string {
+	return fmt.Sprintf(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Network policy viewer</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Network policy viewer</h1>
+      %s
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>`, s)
+}
--- server.go
+++ server.go
@@ -38,7 +38,9 @@ func handleGet(w *http.ResponseWriter, r *http.Request) {
 
 	dot, percentage, err := processBytes([]byte(buffer), &output)
 	if err != nil {
-		dot = err.Error()
+		sData := fmt.Sprintf("<p>Can't process input data: %s</p>", err)
+		fmt.Fprintf(*w, page(sData))
+		return
 	}
 
 	cmd := exec.Command("dot", "-Tsvg")
@@ -47,7 +49,9 @@ func handleGet(w *http.ResponseWriter, r *http.Request) {
 	cmd.Stdout = &svg
 	err = cmd.Run()
 	if err != nil {
-		log.Printf("Graphviz conversion failed: %s\n", err)
+		sDot := fmt.Sprintf("<p>Graphviz conversion failed: %s</p>", err)
+		fmt.Fprintf(*w, page(sDot))
+		return
 	}
 
 	isolatedPercentage := 100 - percentage
@@ -58,35 +62,17 @@ func handleGet(w *http.ResponseWriter, r *http.Request) {
 		colorClass = "progress-bar-warning"
 	}
 
-	// TODO: move to template
-	fmt.Fprintf(*w, `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Network policy viewer</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Network policy viewer</h1>
-      <div>%s</div>
-      <div class="progress">
-        <div class="progress-bar %s" style="width: %d%%" role="progressbar" aria-valuenow="%d" aria-valuemin="0" aria-valuemax="100">%d%% isolated</div>
-      </div>
-    </div>
-  </body>
-</html>`,
+	buffer = fmt.Sprintf(`
+<div>%s</div>
+<div class="progress">
+  <div class="progress-bar %s" style="width: %d%%" role="progressbar" aria-valuenow="%d" aria-valuemin="0" aria-valuemax="100">%d%% isolated</div>
+</div>`,
 		strings.Replace(svg.String(), "Times,serif", "sans-serif", -1),
 		colorClass,
 		isolatedPercentage,
 		isolatedPercentage,
 		isolatedPercentage)
+	fmt.Fprintf(*w, page(buffer))
 }
 
 func handlePost(w *http.ResponseWriter, r *http.Request) {
