7f34cbf4a9c6ecb5bd2a6c6b9a9f2df6d6356596
Gerald Schmidt <gerald1248@gmail.com>
2018-11-16 00:04:51 +0000

    refactored server

--- server.go
+++ server.go
@@ -17,6 +17,10 @@ type PostStruct struct {
 func serve(port int) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler)
+	mux.HandleFunc("/api/", apiHandler)
+	mux.HandleFunc("/api/v1/", apiHandler)
+	mux.HandleFunc("/health", healthHandler)
+	mux.HandleFunc("/api/v1/metrics/", metricsHandler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), mux))
 }
 
@@ -27,6 +31,49 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		handleGet(&w, r)
 	}
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	buffer := fmt.Sprintf(`
+<div class="row">
+  <div class="col-sm-6"><a href="/">/</a></div>
+  <div class="col-sm-6">show graph</div>
+</div>
+<div class="row">
+  <div class="col-sm-6"><a href="/health/">/health/</a></div>
+  <div class="col-sm-6">health endpoint</div>
+</div>
+<div class="row">
+  <div class="col-sm-6"><a href="/api/v1/">/api/v1/</a></div>
+  <div class="col-sm-6">show this page</div>
+</div>
+<div class="row">
+  <div class="col-sm-6"><a href="/api/v1/metrics/">/api/v1/metrics/</a></div>
+  <div class="col-sm-6">metrics endpoint</div>
+</div>`)
+	fmt.Fprintf(w, page(buffer))
+	return
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{\"status\":\"ok\"}")
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+	var buffer, output string
+
+	getJsonData(&buffer)
+
+	output = "dot"
+
+	_, percentage, err := processBytes([]byte(buffer), &output)
+	if err != nil {
+		sData := fmt.Sprintf("<p>Can't process input data: %s</p>", err)
+		fmt.Fprintf(w, page(sData))
+		return
+	}
+
+	fmt.Fprintf(w, "{\"percentage\":\"%d\"}", percentage)
 }
 
 func handleGet(w *http.ResponseWriter, r *http.Request) {
@@ -65,7 +112,7 @@ func handleGet(w *http.ResponseWriter, r *http.Request) {
 	buffer = fmt.Sprintf(`
 <div>%s</div>
 <div class="progress">
-  <div class="progress-bar %s" style="width: %d%%" role="progressbar" aria-valuenow="%d" aria-valuemin="0" aria-valuemax="100">%d%% isolated</div>
+  <div class="progress-bar %s" style="width: %d%%%%" role="progressbar" aria-valuenow="%d" aria-valuemin="0" aria-valuemax="100">%d%%%% isolated</div>
 </div>`,
 		strings.Replace(svg.String(), "Times,serif", "sans-serif", -1),
 		colorClass,
