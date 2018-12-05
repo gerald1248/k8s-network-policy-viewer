67579f3c48ee0ea99f2cf258b5b948ec343b26c7
Gerald Schmidt <gerald1248@gmail.com>
2018-12-05 23:48:05 +0000

    ongoing work on tests

---
+++ select_namespaces_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMatchingNamespace(t *testing.T) {
+	// signature: selectNamespaces(selector *map[string]string, namespacePodMap *map[string][]string, namespaceLabelMap *map[string]map[string]string) []string
+	selector := map[string]string{}
+	selector["app"] = "alice"
+	namespacePodMap := map[string][]string{}
+	nestedArray := []string{}
+	nestedArray = append(nestedArray, "alice")
+	namespacePodMap["alice"] = nestedArray
+	namespaceLabelMap := map[string]map[string]string{}
+	nestedSelector := map[string]string{}
+	nestedSelector["app"] = "alice"
+	namespaceLabelMap["alice"] = nestedSelector
+
+	namespaces := selectNamespaces(&selector, &namespacePodMap, &namespaceLabelMap)
+
+	if len(namespaces) != 1 || namespaces[0] != "alice" {
+		t.Errorf("Must find matching namespace - got array=%v", namespaces)
+	}
+}
